e3050441676b4a1bba8d61811c997bb84940f790
rouzbeh-afrasiabi <rouzbeh.afrasiabi@gmail.com>
2019-10-13 22:53:16 -0400

    Update TrainTorch.py

--- traintorch/TrainTorch.py
+++ traintorch/TrainTorch.py
@@ -237,7 +237,6 @@ class traintorch:
 #                     plt.close(self.parent.figure)
                     clear_output(wait=True)
                     gc.collect()
-                    self.parent.counter+=1
         self._plot=plot(self,)
         self.create=self._plot.create
 
