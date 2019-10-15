1106d0e7c0b24e48afe32d05da71d91029cb857a
rouzbeh-afrasiabi <rouzbeh.afrasiabi@gmail.com>
2019-10-15 19:42:03 -0400

    Update test_one.py

--- tests/test_one.py
+++ tests/test_one.py
@@ -6,13 +6,13 @@ sys.path.insert(0, myPath + '/../')
 from traintorch import *
 
 class TestClass:
-#     def test_one(self):
-#         test=metric('test',w_size=10,average=False,xaxis_int=True,n_ticks=(5, 5))
-#         assert test.name=='test'
-#         assert test.w_size==11
-#         assert test.average==False
-#         assert test.xaxis_int==True
-#         assert test.n_ticks==(5,5)
+    def test_one(self):
+        test=metric('test',w_size=10,average=False,xaxis_int=True,n_ticks=(5, 5))
+        assert test.name=='test'
+        assert test.w_size==11
+        assert test.average==False
+        assert test.xaxis_int==True
+        assert test.n_ticks==(5,5)
 
     def test_two(self):
         x = 1
