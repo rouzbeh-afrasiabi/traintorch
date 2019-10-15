bbab7450f5967fa77dc7435d2e11ad465a3da006
rouzbeh-afrasiabi <rouzbeh.afrasiabi@gmail.com>
2019-10-15 18:10:58 -0400

    Update test_one.py

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,8 +1,14 @@
 import pytest
+from traintorch import *
 
 class TestClass:
     def test_one(self):
-        assert 'cpu'=='cpu'
+    test=metric('test',w_size=10,average=False,xaxis_int=True,n_ticks=(5, 5))
+    assert test.name=='test'
+    assert test.w_size==11
+    assert test.average==False
+    assert test.xaxis_int==True
+    assert test.n_ticks==(5,5)
 
     def test_two(self):
         x = 1
