2121a713a59239792ac98b732cc47627f3fad6bb
rouzbeh-afrasiabi <rouzbeh.afrasiabi@gmail.com>
2019-10-15 20:18:52 -0400

    Update test_one.py

--- tests/test_one.py
+++ tests/test_one.py
@@ -6,7 +6,7 @@ sys.path.insert(0, myPath + '/../')
 from traintorch import *
 
 class TestClass:
-    def test_one(self):
+    def test_metric(self):
         test=metric('test',w_size=10,average=False,xaxis_int=True,n_ticks=(5, 5))
         assert test.name=='test'
         assert test.w_size==11
@@ -14,6 +14,17 @@ class TestClass:
         assert test.xaxis_int==True
         assert test.n_ticks==(5,5)
 
+        for i in range(0,100):
+            pass
+            test.update(x=2*i,t=i,f=3*i)
+        assert all(np.hstack(test.means)==[5,15,16,48,27,81,38,114,49,147,60,180,71,213,82,246,93,279])
+        assert test.counter==100
+        assert test.keys==['x', 't', 'f']
+        assert test.updated==True
+        assert all(np.hstack(test.window().values==[[ 89, 267],[ 90, 270],[ 91, 273],[ 92, 276],[ 93, 279],
+                               [ 94, 282],[ 95, 285],[ 96, 288],[ 97, 291],[ 98, 294],[ 99, 297]]))
+        assert test.x==[198]
+        
     def test_two(self):
         x = 1
         assert x==1
