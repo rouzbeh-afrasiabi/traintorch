f8a543789e83f921c9fd1760cbc4ad1f1e1e769e
rouzbeh-afrasiabi <rouzbeh.afrasiabi@gmail.com>
2019-10-16 23:19:26 -0400

    Update TrainTorch.py

--- traintorch/TrainTorch.py
+++ traintorch/TrainTorch.py
@@ -523,7 +523,7 @@ class collate():
 
     def window(self,):
         temp_a=[]
-        if(str(target_a)=='pycmMetrics' and str(target_b)=='pycmMetrics'):
+        if(str(self.target[0])=='pycmMetrics' and str(self.target[1])=='pycmMetrics'):
             for item_0 in self.target:
                 for item_1 in item_0.metrics:
                     _key=item_1.name.replace(item_0.name+"_","")
