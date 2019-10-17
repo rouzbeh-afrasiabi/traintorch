652774f13b1a41c14a1e0bdfad5b42389f0c3d56
rouzbeh-afrasiabi <rouzbeh.afrasiabi@gmail.com>
2019-10-16 22:30:29 -0400

    Update TrainTorch.py

--- traintorch/TrainTorch.py
+++ traintorch/TrainTorch.py
@@ -309,7 +309,7 @@ class metric:
             self.average=False
             
     def __str__(self,):
-        return self.name
+        return ('metric')
     
     def update(self,**kwargs):
         self.updated=True
@@ -426,7 +426,10 @@ class pycmMetrics():
                     _key=str(key).replace(' ','_')
                     self.metrics_cls[self.name+'_'+str(_key)]=metric(name=self.name+'_'+str(_key),w_size=self.w_size)
         self.metrics=list({**self.metrics_oa,**self.metrics_cls}.values())
-        gc.collect()
+        
+    def __str__(self,):
+        return ('pycmMetrics')
+    
     def _in_list(self,target,main):
         return set(target)<set(main)
     def _to_list(self,target):
@@ -452,7 +455,6 @@ class pycmMetrics():
 
         self.cm_dict_overall=_main
         self.cm_dict_class=_class
-        gc.collect()
         
     def _to_df(self,_cm):
         
@@ -461,7 +463,6 @@ class pycmMetrics():
         self.cm_dict_class=_class
         self.cm_df_overall=pd.DataFrame(_main,index=[0])
         self.cm_df_class=pd.DataFrame(_class,index=[0])
-        gc.collect()
 
     def update(self,actual,predicted):
         _cm=ConfusionMatrix(actual,predicted)
@@ -476,7 +477,6 @@ class pycmMetrics():
                 else:
                     self.metrics_cls[self.name+'_'+str(k)].update(**{k:v})
         self.metrics=list({**self.metrics_oa,**self.metrics_cls}.values())
-        gc.collect()
 
 
 class collate():
@@ -514,7 +514,6 @@ class collate():
                         temp_a.append(pd.concat(item_1.means, axis=1).T)
         if(temp_a):
             self.means=pd.concat(temp_a,axis=1)
-        gc.collect()
     def window(self,):
         temp_a=[]
         for item_0 in self.target:
@@ -523,5 +522,4 @@ class collate():
                 if(_key==self.target_metric):
                     temp_a.append(item_1.window())
         self.update()
-        gc.collect()
         return(pd.concat(temp_a,axis=1))    
