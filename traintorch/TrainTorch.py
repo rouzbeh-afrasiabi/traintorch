c0e50fe24e1bdf0e2a2f5f79ed303087d276b212
rouzbeh-afrasiabi <rouzbeh.afrasiabi@gmail.com>
2019-10-17 16:35:33 -0400

    Update TrainTorch.py

--- traintorch/TrainTorch.py
+++ traintorch/TrainTorch.py
@@ -77,6 +77,7 @@ class traintorch:
         self.counter=0
         self.window=window
         self.custom_metrics=[]
+        self.custom_tbl_metrics=[]
         self.main_results=pd.DataFrame()
         self.custom_data=None
         self.total_plots=0
@@ -93,14 +94,20 @@ class traintorch:
 
         self._avg_axes=[]
         
-    def append(self,target):
+    def append(self,plot_targets,table_targets):
         temp=[]
-        for item in target:
+        for item in plot_targets:
             if(isinstance(item,(metric,collate))):
                 temp.append(item)
             elif(isinstance(item,pycmMetrics)):
                 temp+=item.metrics
         self.custom_metrics=temp
+        
+        temp=[]
+        for item in table_targets:
+            if(isinstance(item,metricsTable)):
+                temp.append(item)
+        self.custom_tbl_metrics=temp
 
         class plot:
             def __init__(self,parent,):
@@ -554,5 +561,28 @@ class collate():
             if(not self.target[0].means or not self.target[1].means):
                 return pd.DataFrame([0,0,0,0],columns=['No Data Available Yet'])
             else:
-                return pd.concat([pd.concat(item.means,axis=1) for item in self.target],axis=0).T                
-            
+                return pd.concat([pd.concat(item.means,axis=1) for item in self.target],axis=0).T   
+
+class metricsTable:
+    def __init__(self,):
+        self.counter=0
+        self.updated=False
+        self.__kwargs=None
+        self.keys=[]
+        
+    def window(self,):
+        _dict={}
+        for k,v in self.__dict__.items():
+            if(k in self.__dict__['keys']):
+                _dict[k]=v
+        _data=pd.DataFrame(_dict)
+        return(_data)
+        
+    def update(self,**kwargs):
+        self.updated=True
+        self.counter+=1
+        self.__kwargs=kwargs
+        for key in self.__kwargs.keys():
+            self.__dict__[key]=[self.__kwargs[key]]
+            if(key not in self.keys):
+                self.keys.append(key)            
